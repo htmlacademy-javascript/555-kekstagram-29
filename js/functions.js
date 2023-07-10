355147e76fccfa844d690b37ae6d00b4fc36603f
Etel Bogen <etel.bogen@gmail.com>
2023-07-10 23:15:35 +0300

    checked function

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+const getCheckStringLength = (string, lengthLimit) => string.length <= lengthLimit;
+
+getCheckStringLength('проверяемая строка', 10);
+
+
+const getCheckStingIsPalindrome = (rawString) => {
+  const string = rawString.replaceAll(' ', '').toLowerCase();
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string.at(i) !== string.at(-i - 1)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+getCheckStingIsPalindrome('Лёша на полке клопа нашёл ');
+getCheckStingIsPalindrome('Это просто строка для проверки');
